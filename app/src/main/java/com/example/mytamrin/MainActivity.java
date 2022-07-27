05e52ffdbd9640a63ad18b0196642d5f5814af6f
Abbas <absgorji@gmail.com>
2022-07-27 17:21:13 +0430

    Creat Project

--- app/src/main/java/com/example/mytamrin/MainActivity.java
+++ app/src/main/java/com/example/mytamrin/MainActivity.java
@@ -56,6 +56,7 @@ int i;
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
               String set1 = Ename.getText().toString().trim();
